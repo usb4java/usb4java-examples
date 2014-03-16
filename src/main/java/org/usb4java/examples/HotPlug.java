1372efe1361ad3253df9498e0bb8872fef5c4e01
Klaus Reimer <k@ailis.de>
2014-03-16 20:08:29 +0100

    Use handleEventTimeout instead of handleEvent and improve error handling

--- src/main/java/org/usb4java/examples/HotPlug.java
+++ src/main/java/org/usb4java/examples/HotPlug.java
@@ -21,15 +21,15 @@ import org.usb4java.LibUsbException;
 public class HotPlug
 {
     /**
-     * This is the event handling thread. libusb doesn't start threads by
-     * its own so it is our own responsibility to give libusb time to handle
-     * the events in our own thread.
+     * This is the event handling thread. libusb doesn't start threads by its
+     * own so it is our own responsibility to give libusb time to handle the
+     * events in our own thread.
      */
     static class EventHandlingThread extends Thread
     {
         /** If thread should abort. */
         private volatile boolean abort;
-        
+
         /**
          * Aborts the event handling thread.
          */
@@ -37,19 +37,23 @@ public class HotPlug
         {
             this.abort = true;
         }
-        
+
         @Override
         public void run()
         {
             while (!this.abort)
             {
-                // This call blocks until libusb has handled at least one
-                // event or a hotplug callback has been deregistered 
-                LibUsb.handleEvents(null);
+                // Let libusb handle pending events. This blocks until events
+                // have been handled, a hotplug callback has been deregistered
+                // or the specified time of 1 second (Specified in
+                // Microseconds) has passed.
+                int result = LibUsb.handleEventsTimeout(null, 1000000);
+                if (result != LibUsb.SUCCESS)
+                    throw new LibUsbException("Unable to handle events", result);
             }
         }
     }
-    
+
     /**
      * The hotplug callback handler
      */
@@ -60,9 +64,12 @@ public class HotPlug
             Object userData)
         {
             DeviceDescriptor descriptor = new DeviceDescriptor();
-            LibUsb.getDeviceDescriptor(device, descriptor);
-            System.out.format("%s: %04x:%04x%n", 
-                event == LibUsb.HOTPLUG_EVENT_DEVICE_ARRIVED ? "Connected" : 
+            int result = LibUsb.getDeviceDescriptor(device, descriptor);
+            if (result != LibUsb.SUCCESS)
+                throw new LibUsbException("Unable to read device descriptor",
+                    result);
+            System.out.format("%s: %04x:%04x%n",
+                event == LibUsb.HOTPLUG_EVENT_DEVICE_ARRIVED ? "Connected" :
                     "Disconnected",
                 descriptor.idVendor(), descriptor.idProduct());
             return 0;
@@ -85,7 +92,7 @@ public class HotPlug
         {
             throw new LibUsbException("Unable to initialize libusb", result);
         }
-        
+
         // Check if hotplug is available
         if (!LibUsb.hasCapability(LibUsb.CAP_HAS_HOTPLUG))
         {
@@ -109,16 +116,16 @@ public class HotPlug
             new Callback(), null, callbackHandle);
         if (result != LibUsb.SUCCESS)
         {
-            throw new LibUsbException("Unable to register hotplug callback", 
+            throw new LibUsbException("Unable to register hotplug callback",
                 result);
-        }        
+        }
 
         // Our faked application. Hit enter key to exit the application.
         System.out.println("Hit enter to exit the demo");
         System.in.read();
-        
+
         // Unregister the hotplug callback and stop the event handling thread
-        thread.abort();        
+        thread.abort();
         LibUsb.hotplugDeregisterCallback(null, callbackHandle);
 
         // Deinitialize the libusb context
